733ef4884cfd49002d9887f0d7c4cd42cf82647e
Justin Ong <justin.ong.97@gmail.com>
2016-10-15 08:04:13 +0800

    added solenoid control on falling edge of CH5

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,6 +1,10 @@
 #include "Receiver.h"
 #include "Motors.h"
 
+#define SOLENOID_PULSE_LENGTH 50
+#define SOLENOID_PIN 6
+
+elapsedMillis solenoid_active;
 void setup() {
   Serial1.begin(115200);
   Serial3.begin(115200);
@@ -8,8 +12,14 @@ void setup() {
 
   Motors.begin();
   Receiver.begin();
+  pinMode(SOLENOID_PIN, OUTPUT);
   pinMode(13, OUTPUT);
 
+  pinMode(2, OUTPUT);
+  pinMode(5, OUTPUT); 
+
+  solenoid_active = SOLENOID_PULSE_LENGTH + 1;
+  
   Serial1.println("Setup finished!");
 }
 
@@ -19,6 +29,7 @@ void loop() {
   static float theta = 0;
   static float theta_offset = 0;
   static uint8_t pCH5 = 2;
+  uint8_t CH5 = Receiver.get_channel(5);
   
   if (last_printed > 100) {
     last_printed = 0;
@@ -34,8 +45,26 @@ void loop() {
     digitalWrite(13, !digitalRead(13));
   }
 
+  if (solenoid_active < SOLENOID_PULSE_LENGTH) {
+    digitalWrite(SOLENOID_PIN, HIGH);
+  }
+  else {
+    digitalWrite(SOLENOID_PIN, LOW);
+  }
+
+  if (CH5 == 1 && pCH5 == 0) {
+    // if switch was just pulled down
+
+    theta_offset = theta;
+    solenoid_active = 0;
+  }
+
   if (last_motor_set > 50) {
-    float theta_rads = theta * 71 / 4068;
+    float theta_corrected = theta - theta_offset;
+
+    if (theta_corrected < 0) theta_corrected += 360;
+    
+    float theta_rads = theta_corrected * 71 / 4068;
     last_motor_set = 0;
 
     int16_t forward = Receiver.get_channel(4);
@@ -48,9 +77,23 @@ void loop() {
     Motors.set_power(Motors.FrontRight, - new_forward + new_right + Receiver.get_channel(1));
     Motors.set_power(Motors.BackLeft, new_forward - new_right + Receiver.get_channel(1));
     Motors.set_power(Motors.BackRight, - new_forward - new_right + Receiver.get_channel(1));
+
+    if (Receiver.get_channel(3) > 70) {
+      digitalWrite(2, HIGH);
+      digitalWrite(5, LOW);
+    }
+    else if (Receiver.get_channel(3) < -70  ) {
+      digitalWrite(2, LOW);
+      digitalWrite(5, HIGH);
+    }
+    else {
+      digitalWrite(2, LOW);
+      digitalWrite(5, LOW);
+    }
   }
   
   if (Serial3.findUntil("Headings  ", "\n\r")) {
     theta = Serial3.parseFloat();
   }
+  pCH5 = CH5;
 }
